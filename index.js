9f4b061bf854f2a3bf1060c9af12d4302ff93a70
AZIHubert <allan.jouannet@protonmail.com>
2020-06-29 14:40:05 +0200

    fixe mongoose depreciation

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ const connect = async () => {
     try{
         await mongoose.connect(MONGODB, {
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
+            useCreateIndex: true
         });
         console.log('Connected to mongo ATLAS');
         app.listen({ port }, () => console.log(`Server ready at http://localhost:${port}${server.graphqlPath}`));
