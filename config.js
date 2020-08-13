6b19d2839a8788674f27795993ec7ec952e361f0
AZIHubert <allan.jouannet@protonmail.com>
2020-08-14 00:14:32 +0200

    deployment

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://AZIHubert:Aaoudjiuvhds9@cluster0-1kkuw.mongodb.net/Cluster0?retryWrites=true&w=majority',
+    PASSWORDREGISTRATION: 'alkfha987kjhaflkefhja980kajfafabj98790kjald',
+    ADMINREGISTRATION: '09876gkjh98097987lkejgmhjopdfiqsng0978786lkjLKHJj098kKJN',
+    SECRET: 'lasjfnlakefzg887kjfkjah98785897kajfhkajfgakjzh',
+    SECRET2: 'falkfhaLKLKJ87kanfkjbaKJGLç88798JHkjhfkjhkjfh98fkjhKJH',
+    CLOUDINARY_API_KEY: '822246234369443',
+	CLOUDINARY_API_SECRET: 'djKg_PNq9wrbj0d1fCFs_u18YZU',
+	CLOUDINARY_CLOUD_NAME: 'dy85d5ei6',
+	CLOUDINARY_UPLOAD_PRESET: 'afzb86yl',
+};
